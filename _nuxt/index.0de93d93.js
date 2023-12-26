99896278a605571e14013937b76607d0d9084dab
Nikita Logos <logosmila@gmail.com>
2023-12-27 01:34:08 +0200

    deploy

---
+++ _nuxt/index.0de93d93.js
@@ -0,0 +1,1 @@
+import{i as e,j as t}from"./entry.5882b8c8.js";const n=t({created(){this.$router.push("/en/about")}});function o(r,s,a,c,p,u){return null}const _=e(n,[["render",o]]);export{_ as default};
